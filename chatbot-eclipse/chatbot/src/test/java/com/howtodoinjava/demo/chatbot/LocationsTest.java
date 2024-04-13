e2d19885d6ddf4c952c5ea6abae162d6d4f60d38
Puvatida Simcharoen <puvatida.simcharoen@student.griffith.ie>
2024-04-13 15:18:20 +0100

    created a junit4 LocationTest class

---
+++ chatbot-eclipse/chatbot/src/test/java/com/howtodoinjava/demo/chatbot/LocationsTest.java
@@ -0,0 +1,19 @@
+package com.howtodoinjava.demo.chatbot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LocationsTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
