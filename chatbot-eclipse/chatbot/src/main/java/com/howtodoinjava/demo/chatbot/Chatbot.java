630349141aed62190fab14a289516fb8c5676afb
osama0211 <o.samat0211@gmail.com>
2024-04-15 16:05:24 +0100

    Added get api requests

--- chatbot-eclipse/chatbot/src/main/java/com/howtodoinjava/demo/chatbot/Chatbot.java
+++ chatbot-eclipse/chatbot/src/main/java/com/howtodoinjava/demo/chatbot/Chatbot.java
@@ -1,26 +1,15 @@
 package com.howtodoinjava.demo.chatbot;
 
-
 import java.io.File;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.Scanner;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import org.alicebot.ab.Bot;
-import org.alicebot.ab.Chat;
-import org.alicebot.ab.History;
-import org.alicebot.ab.MagicBooleans;
-import org.alicebot.ab.MagicStrings;
-import org.alicebot.ab.utils.IOUtils;
-
-
-import java.io.File;
-
 public class Chatbot {
-
     // RapidAPI Key
     private static final String RAPID_API_KEY = "b47b0922e4msh19452d17bed706cp1ad4bdjsn6a8c20371cb6";
 
@@ -28,7 +17,7 @@ public class Chatbot {
     private static final String RAPID_API_HOST = "visual-crossing-weather.p.rapidapi.com";
 
     public static void main(String[] args) {
-       {
+        try {
             Scanner scanner = new Scanner(System.in);
             String[] locations = new String[5];
 
@@ -38,4 +27,48 @@ public class Chatbot {
                 locations[i] = scanner.nextLine();
             }
 
-       }}}
+            // loop for each location
+            for (String location : locations) {
+                // Call API
+                String weatherResponse = getWeatherResponse(location);
+
+                // Print temperatures for the next three days, since we get location for 16 days from API
+                printTemperaturesForNextThreeDays(location, weatherResponse);
+            }
+
+            scanner.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printTemperaturesForNextThreeDays(String location, String weatherResponse) {
+        // Split by line
+
+        System.out.println(weatherResponse);
+    }
+
+    private static String getWeatherResponse(String location) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        // Encode the location
+        String encodedLocation = URLEncoder.encode(location, "UTF-8");
+
+        Request request = new Request.Builder()
+                .url("https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location="
+                        + encodedLocation + "&contentType=csv&unitGroup=us&shortColumnNames=0")
+                .get()
+                .addHeader("X-RapidAPI-Key", RAPID_API_KEY)
+                .addHeader("X-RapidAPI-Host", RAPID_API_HOST)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                
+                return response.body().string();
+            } else {
+                throw new IOException("error code: " + response);
+            }
+        }
+    }
+}
