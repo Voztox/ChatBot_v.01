44c0cdcb72830b611a3024bbb73067772c990260
osama0211 <o.samat0211@gmail.com>
2024-04-15 15:50:56 +0100

    added 5 location entries to chatbot

--- chatbot-eclipse/chatbot/src/main/java/com/howtodoinjava/demo/chatbot/Chatbot.java
+++ chatbot-eclipse/chatbot/src/main/java/com/howtodoinjava/demo/chatbot/Chatbot.java
@@ -1,78 +1,30 @@
 package com.howtodoinjava.demo.chatbot;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
-import org.alicebot.ab.Bot;
-import org.alicebot.ab.Chat;
-import org.alicebot.ab.History;
-import org.alicebot.ab.MagicBooleans;
-import org.alicebot.ab.MagicStrings;
-import org.alicebot.ab.utils.IOUtils;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Chatbot {
-	private static final boolean TRACE_MODE = false;
-	static String botName = "super";
-	
-	public static void main(String[] args) {
-		
-		try {
+    // RapidAPI Key
+    private static final String RAPID_API_KEY = "b47b0922e4msh19452d17bed706cp1ad4bdjsn6a8c20371cb6";
 
-			String resourcesPath = getResourcesPath();
-			System.out.println(resourcesPath);
-			MagicBooleans.trace_mode = TRACE_MODE;
-			Bot bot = new Bot("super", resourcesPath);
-			Chat chatSession = new Chat(bot);
-			bot.brain.nodeStats();
-			String textLine = "";
+    // RapidAPI Host
+    private static final String RAPID_API_HOST = "visual-crossing-weather.p.rapidapi.com";
 
-			while (true) {
-				System.out.print("Human : ");
-				textLine = IOUtils.readInputTextLine();
-				
-				if ((textLine == null) || (textLine.length() < 1))
-					textLine = MagicStrings.null_input;
-				
-				if (textLine.equals("q")) {
-					System.exit(0);
-				} 
-				
-				else if (textLine.equals("wq")) {
-					bot.writeQuit();
-					System.exit(0);
-				} 
-				
-				else {
-					String request = textLine;
-					
-					if (MagicBooleans.trace_mode)
-						System.out.println(
-								"STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0)
-										+ ":TOPIC=" + chatSession.predicates.get("topic"));
-					String response = chatSession.multisentenceRespond(request);
-					while (response.contains("&lt;"))
-						response = response.replace("&lt;", "<");
-					while (response.contains("&gt;"))
-						response = response.replace("&gt;", ">");
-					System.out.println("Robot : " + response);
-				}
-			}
-		}//try 
-		catch (Exception e) {
-			e.printStackTrace();
-		}//catch
-	
-	}//main
+    public static void main(String[] args) {
+       {
+            Scanner scanner = new Scanner(System.in);
+            String[] locations = new String[5];
 
-	private static String getResourcesPath() {
-		
-		File currDir = new File(".");
-		String path = currDir.getAbsolutePath();
-		path = path.substring(0, path.length() - 2);
-		System.out.println(path);
-		String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
-		return resourcesPath;
-		
-	}//getResourcePath()
-}//class
+            // Ask the user to enter 5 locations
+            for (int i = 0; i < 5; i++) {
+                System.out.print("Enter location " + (i + 1) + ": ");
+                locations[i] = scanner.nextLine();
+            }
 
+       }}}
 
