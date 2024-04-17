6a72bcdd912b008c0b1eb9e6c7adb62ce87d8cd2
MertFarukGunes <mertfaruk.ie@gmail.com>
2024-04-17 10:37:55 +0100

    Fixed the issue with ChatbotGUI

--- chatbot-eclipse/chatbot/src/main/java/com/howtodoinjava/demo/chatbot/ChatbotGUI.java
+++ chatbot-eclipse/chatbot/src/main/java/com/howtodoinjava/demo/chatbot/ChatbotGUI.java
@@ -3,88 +3,163 @@ package com.howtodoinjava.demo.chatbot;
 import org.alicebot.ab.Bot;
 import org.alicebot.ab.Chat;
 import org.alicebot.ab.MagicBooleans;
-
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.net.URLEncoder;
+import java.io.IOException;
 
 public class ChatbotGUI extends JFrame {
-	private JTextArea conversationArea;
-	private JTextField inputField;
-	private Chat chatSession;
+    // RapidAPI Key
+    private static final String RAPID_API_KEY = "b47b0922e4msh19452d17bed706cp1ad4bdjsn6a8c20371cb6";
 
-	public ChatbotGUI(Chat chatSession) {
-		this.chatSession = chatSession;
-		setTitle("NoName"); // name of our chatbot
-		setSize(850, 250); // size of the jFrame
-		setDefaultCloseOperation(EXIT_ON_CLOSE); // close operation
-		setLayout(new BorderLayout()); // borderlayout
+    // RapidAPI Host
+    private static final String RAPID_API_HOST = "visual-crossing-weather.p.rapidapi.com";
 
-		// dimension takes the size of the screen
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private JTextArea conversationArea;
+    private JTextField inputField;
+    private Chat chatSession;
+    private int locationIndex = 0;
+    private String[] locations = new String[5];
 
-		// centering the screen
-		int x = (screenSize.width - getWidth()) / 2;
-		int y = (screenSize.height - getHeight()) / 2;
+    public ChatbotGUI(Chat chatSession) {
+        this.chatSession = chatSession;
+        setTitle("NoName");
+        setSize(850, 250);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
 
-		// Set the location of the JFrame to the calculated coordinates
-		setLocation(x, y);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (screenSize.width - getWidth()) / 2;
+        int y = (screenSize.height - getHeight()) / 2;
+        setLocation(x, y);
 
-		conversationArea = new JTextArea(); //Jtextare for conversation to display
-		conversationArea.setEditable(false);
-		conversationArea.setFont(new Font("Arial", Font.PLAIN, 24)); // text font changes 
-		JScrollPane scrollPane = new JScrollPane(conversationArea); // scrollbar when the given space is not enough
-		add(scrollPane, BorderLayout.CENTER); // centering the jTextArea
+        conversationArea = new JTextArea();
+        conversationArea.setEditable(false);
+        conversationArea.setFont(new Font("Arial", Font.PLAIN, 24));
+        JScrollPane scrollPane = new JScrollPane(conversationArea);
+        add(scrollPane, BorderLayout.CENTER);
 
-		inputField = new JTextField(); //User input
-		inputField.setFont(new Font("Arial", Font.PLAIN, 24));
-		inputField.addActionListener(new ActionListener() { 
-			@Override
-			public void actionPerformed(ActionEvent e) { 
-				String input = inputField.getText();
-				addMessage("You", input); // display for the user input
-				inputField.setText("");
-				String response = chatSession.multisentenceRespond(input);
-				addMessage("noName Chatbot", response); //display for the bot's input
-			}
-		});
-		add(inputField, BorderLayout.SOUTH); //jFrame for the bottom of the display
-		
-		sendInitialMessage();
-		
-		setVisible(true);
-	}
+        inputField = new JTextField();
+        inputField.setFont(new Font("Arial", Font.PLAIN, 24));
+        inputField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = inputField.getText();
+                addMessage("You", input);
+                inputField.setText("");
 
-	private void sendInitialMessage() {
-        String initialMessage = "Hello, which city are you gonna be in?"; // Initial message from the chatbot
-        addMessage("NoName Chatbot", initialMessage); // Display initial message
+                if (locationIndex < 5) {
+                    locations[locationIndex++] = input;
+                    if (locationIndex == 5) {
+                        fetchWeatherAndSuggestClothing();
+                    } else {
+                        askForNextLocation();
+                    }
+                }
+            }
+        });
+        add(inputField, BorderLayout.SOUTH);
+
+        askForNextLocation();
+
+        setVisible(true);
     }
 
-	
-	private void addMessage(String sender, String message) {
-		conversationArea.append(sender + ": " + message + "\n"); // messaging method
-	}
+    private void askForNextLocation() {
+        conversationArea.append("Please enter destination " + (locationIndex + 1) + ": ");
+    }
 
-	public static void main(String[] args) {
-		try {
-			String resourcesPath = getResourcesPath();
-			MagicBooleans.trace_mode = false;
-			Bot bot = new Bot("super", resourcesPath);
-			Chat chatSession = new Chat(bot);
-			new ChatbotGUI(chatSession);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private void fetchWeatherAndSuggestClothing() {
+        for (String location : locations) {
+            addMessage("NoName Chatbot", "Fetching weather for " + location);
+            try {
+                String weatherResponse = getWeatherResponse(location);
+                printTemperaturesForNextThreeDays(location, weatherResponse);
+                double currentTemperature = getCurrentTemperature(weatherResponse);
+                String clothingSuggestion = suggestClothing(currentTemperature);
+                addMessage("NoName Chatbot", "Clothing suggestion for " + location + ": " + clothingSuggestion);
+            } catch (Exception e) {
+                addMessage("NoName Chatbot", "Error fetching weather for " + location);
+            }
+        }
+    }
 
-	private static String getResourcesPath() {
-		File currDir = new File(".");
-		String path = currDir.getAbsolutePath();
-		path = path.substring(0, path.length() - 2);
-		String resourcesPath = path + File.separator + "src"
-				+ File.separator + "main" + File.separator + "resources";
-		return resourcesPath;
-	}
+    private void printTemperaturesForNextThreeDays(String location, String weatherResponse) {
+        String[] splitter = weatherResponse.split("\n");
+        conversationArea.append("Temperatures for " + location + ":\n");
+        for (int i = 1; i <= 3 && i < splitter.length; i++) {
+            String[] data = splitter[i].split(",");
+            double fahrenheit = Double.parseDouble(data[9]);
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            conversationArea.append("Day " + i + ": " + celsius + " degrees Celsius\n");
+        }
+        conversationArea.append("\n");
+    }
+
+    private double getCurrentTemperature(String weatherResponse) {
+        String[] splitter = weatherResponse.split("\n");
+        String[] data = splitter[1].split(",");
+        double fahrenheit = Double.parseDouble(data[9]);
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    private String getWeatherResponse(String location) throws Exception {
+        OkHttpClient client = new OkHttpClient();
+        String encodedLocation = URLEncoder.encode(location, "UTF-8");
+        Request request = new Request.Builder()
+                .url("https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location="
+                        + encodedLocation + "&contentType=csv&unitGroup=us&shortColumnNames=0")
+                .get()
+                .addHeader("X-RapidAPI-Key", RAPID_API_KEY)
+                .addHeader("X-RapidAPI-Host", RAPID_API_HOST)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                return response.body().string();
+            } else {
+                throw new IOException("Error " + response.code());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            String resourcesPath = getResourcesPath();
+            MagicBooleans.trace_mode = false;
+            Bot bot = new Bot("super", resourcesPath);
+            Chat chatSession = new Chat(bot);
+            new ChatbotGUI(chatSession);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getResourcesPath() {
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        path = path.substring(0, path.length() - 2);
+        String resourcesPath = path + File.separator + "src"
+                + File.separator + "main" + File.separator + "resources";
+        return resourcesPath;
+    }
+
+    private void addMessage(String sender, String message) {
+        conversationArea.append(sender + ": " + message + "\n");
+    }
+
+    public static String suggestClothing(double temperature) {
+        if (temperature < 5) {
+            return "You should wear a light jacket.";
+        } else if (temperature < 17) {
+            return "You should wear a shirt.";
+        } else {
+            return "You should wear Borat's mankini";
+        }
+    }
 }
