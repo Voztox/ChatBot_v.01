5b4c670e571bd3d7284839beb931335b506598a5
MertFarukGunes <mertfaruk.ie@gmail.com>
2024-04-30 14:48:19 +0100

    ChatbotGUITest got created

---
+++ chatbot-eclipse/chatbot/src/test/java/com/howtodoinjava/demo/chatbot/ChatbotGUITest.java
@@ -0,0 +1,14 @@
+package com.howtodoinjava.demo.chatbot;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChatbotGUITest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
