588cdcc39f6d70a51fa7271b6f4d61554d07ccc5
Puvatida <165152053+Puvatida@users.noreply.github.com>
2024-05-01 00:26:06 +0100

    wrote lightRainSixtyPercent() test; fail.
    fail because method in main class isn't written yet.

--- chatbot-eclipse/chatbot/src/test/java/com/howtodoinjava/demo/chatbot/ChatbotTest.java
+++ chatbot-eclipse/chatbot/src/test/java/com/howtodoinjava/demo/chatbot/ChatbotTest.java
@@ -331,7 +331,14 @@ public class ChatbotTest {
 	}
 	@Test
 	void lightRainSixtyPercent() { //40-60% is a moderate to light shower rainfall. 
-		
+		//mock weather response: in percentage of rain:
+		String precipitation = "60.0";
+								
+		//calling the getPrecipitationPercentage method in chatbot. 
+		String chanceOfRain = Chatbot.getPrecipitationPercentage(precipitation);
+		//assert Equal the results. 
+		Assert.assertEquals("There will be a moderate light shower", chanceOfRain);
+
 	}
 	
 	
